3885aa34989d0becadcb4b1a18519bd3e0029872
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2021-11-18 19:04:13 +0900

    cmake: add predefined macro ASMLANG for separating assembler part

--- cmake/gcc.cmake
+++ cmake/gcc.cmake
@@ -1,6 +1,10 @@
 # SPDX-License-Identifier: Apache-2.0
 
-prj_compile_options(-Wall)
+# Common
+prj_compile_options(-g -Wall)
+
+# Assembler
+prj_compile_options($<$<COMPILE_LANGUAGE:ASM>:-DASMLANG>)
 
 if("${ARCH}" STREQUAL "riscv")
   include(${CMAKE_SOURCE_DIR}/cmake/gcc_riscv.cmake)
