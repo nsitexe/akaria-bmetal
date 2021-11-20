08eb8df97b09b04c9cd35390ac2cf6ecf5b8a8c9
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2021-11-20 13:13:08 +0900

    sys: show unknown syscall number in the warning

--- sys/syscall.c
+++ sys/syscall.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 
 #include <errno.h>
+#include <inttypes.h>
 #include <stdint.h>
 
 #include <bmetal/printk.h>
@@ -8,7 +9,7 @@
 
 intptr_t __sys_unknown(intptr_t number, intptr_t a, intptr_t b, intptr_t c, intptr_t d, intptr_t e, intptr_t f)
 {
-	printk("unknown syscall\n");
+	printk("unknown syscall %"PRIdPTR"\n", number);
 
 	return -ENOTSUP;
 }
