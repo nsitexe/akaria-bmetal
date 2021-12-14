9977b0acf0e8a93b7dcc4d29f0892f0373518d29
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2021-12-15 01:47:31 +0900

    boards: riscv: qemu-virt: add 4 cpu configs

--- boards/riscv/qemu-virt/board.c
+++ boards/riscv/qemu-virt/board.c
@@ -2,7 +2,64 @@
 
 #include <bmetal/device.h>
 #include <bmetal/init.h>
+#include <bmetal/drivers/cpu.h>
 #include <bmetal/drivers/uart.h>
+
+const static struct __device_config cpu0_conf[] = {
+	{"hartid", 0},
+	{0, 0},
+};
+
+const static struct __device_config cpu1_conf[] = {
+	{"hartid", 1},
+	{0, 0},
+};
+
+const static struct __device_config cpu2_conf[] = {
+	{"hartid", 2},
+	{0, 0},
+};
+
+const static struct __device_config cpu3_conf[] = {
+	{"hartid", 3},
+	{0, 0},
+};
+
+static struct __cpu_device cpu0 = {
+	.base = {
+		.name = "cpu0",
+		.type_vendor = "generic",
+		.type_device = "cpu_riscv",
+		.conf = cpu0_conf,
+	},
+};
+
+static struct __cpu_device cpu1 = {
+	.base = {
+		.name = "cpu1",
+		.type_vendor = "generic",
+		.type_device = "cpu_riscv",
+		.conf = cpu1_conf,
+	},
+};
+
+static struct __cpu_device cpu2 = {
+	.base = {
+		.name = "cpu2",
+		.type_vendor = "generic",
+		.type_device = "cpu_riscv",
+		.conf = cpu2_conf,
+	},
+};
+
+static struct __cpu_device cpu3 = {
+	.base = {
+		.name = "cpu3",
+		.type_vendor = "generic",
+		.type_device = "cpu_riscv",
+		.conf = cpu3_conf,
+	},
+};
 
 const static struct __device_config uart0_conf[] = {
 	{"reg", 0x10000000},
@@ -22,6 +79,10 @@ static struct __uart_device uart0 = {
 
 static int board_qemu_virt_init(void)
 {
+	__cpu_add_device(&cpu0, __bus_get_root());
+	__cpu_add_device(&cpu1, __bus_get_root());
+	__cpu_add_device(&cpu2, __bus_get_root());
+	__cpu_add_device(&cpu3, __bus_get_root());
 	__uart_add_device(&uart0, __bus_get_root(), 1);
 
 	return 0;
