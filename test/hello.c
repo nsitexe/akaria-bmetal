b554fcd645922362bf752ad57cc17ded4235db6f
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2022-01-20 23:24:54 +0900

    test: dump argc and argv

--- test/hello.c
+++ test/hello.c
@@ -6,5 +6,13 @@
 int main(int argc, char *argv[], char *envp[])
 {
 	printf("hello world!\n");
+
+	printf("value of argv[%d]: %d\n", 4, *((int *)argv[4]));
+
+	printf("argc:%d\n", argc);
+	for (int i = 0; i < argc; i++) {
+		printf("argv[%d]: %p\n", i, argv[i]);
+	}
+
 	return 0;
 }
