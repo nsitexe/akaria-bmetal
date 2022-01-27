61cf16c2a6ed292af0388c2c9adb71d585468a4e
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2022-01-27 21:22:25 +0900

    test: avoid to change sysroot directory

--- test/bmetal.mk
+++ test/bmetal.mk
@@ -7,11 +7,14 @@ ifeq ($(strip $(USE_NEWLIB)$(USE_MUSL)),)
 endif
 
 # Make additional CFLAGS, LDFLAGS
-BAREMETAL_CMNFLAGS = -static -nostdlib --sysroot=$(USE_SYSROOT)
+BAREMETAL_CMNFLAGS = \
+	-static -nostdlib \
+	-I $(USE_SYSROOT)/include
 BAREMETAL_CMNLDFLAGS = \
 	-Wl,-T,generated/linker_gen.ld \
 	-Wl,--print-memory-usage \
-	-L $(USE_SYSROOT)/include/bmetal
+	-L $(USE_SYSROOT)/include/bmetal \
+	-L $(USE_SYSROOT)/lib
 
 LDADD += -Wl,--whole-archive,-lbmetal_crt,--no-whole-archive
 ifeq ($(USE_NEWLIB),y)
@@ -25,5 +28,11 @@ ifeq ($(USE_MUSL),y)
   CFLAGS   += $(BAREMETAL_CMNFLAGS)
   LDFLAGS  += $(BAREMETAL_CMNFLAGS) $(BAREMETAL_CMNLDFLAGS)
   LDADD    += -lc
+endif
+ifeq ($(USE_GLIBC),y)
+  CPPFLAGS +=
+  CFLAGS   += $(BAREMETAL_CMNFLAGS)
+  LDFLAGS  += $(BAREMETAL_CMNFLAGS) $(BAREMETAL_CMNLDFLAGS)
+  LDADD    += -lc -lgcc_eh -lc
 endif
 LDADD += -lgcc
