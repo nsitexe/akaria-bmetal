66f986ffe89d4a95198996ac4e469a8ec06abf51
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2022-01-31 23:50:09 +0900

    cmake: add options for each build targets

--- cmake/gcc.cmake
+++ cmake/gcc.cmake
@@ -1,7 +1,13 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # Common
-prj_compile_options(-g -Wall)
+prj_compile_options(
+  -Wall
+  $<$<CONFIG:Debug>:>
+  $<$<CONFIG:Release>:>
+  $<$<CONFIG:MinSizeRel>:>
+  $<$<CONFIG:RelWithDebInfo>:>
+)
 
 # Assembler
 prj_compile_options($<$<COMPILE_LANGUAGE:ASM>:-DASMLANG>)
