a3a89e3536f8a8efa3785d6eb19f43eae541aa8d
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2022-02-04 00:28:30 +0900

    test: remove argv dump code from vecadd

--- test/vecadd.c
+++ test/vecadd.c
@@ -5,18 +5,18 @@ void vecadd(const double *a, const double *b, double *c, int n)
 {
 	for (int i = 0; i < n; i++) {
 		c[i] = a[i] + b[i];
+
+		if (i < 10) {
+			printf("%d: a(%f) + b(%f) = c(%f)\n", i, a[i], b[i], c[i]);
+		}
 	}
 }
 
 int main(int argc, char *argv[], char *envp[])
 {
-	printf("%s: hello world!\n", argv[0]);
+	printf("%s: vecadd start\n", argv[0]);
 
 	printf("argc: %d\n", argc);
-	for (int i = 0; i < argc; i++) {
-		printf("argv[%d]: %p\n", i, argv[i]);
-	}
-
 	if (argc > 4) {
 		double *a = (double *)argv[1];
 		double *b = (double *)argv[2];
