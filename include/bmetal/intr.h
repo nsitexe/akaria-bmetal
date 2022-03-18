98c2ae56084fadcaf48770e230322b802ae9f4a8
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2022-03-18 17:09:28 +0900

    sys: add interrupt control API

---
+++ include/bmetal/intr.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#ifndef BAREMETAL_CRT_INTR_H_
+#define BAREMETAL_CRT_INTR_H_
+
+#include <bmetal/bmetal.h>
+
+void __intr_enable_local(void);
+void __intr_disable_local(void);
+void __intr_restore_local(const long status);
+void __intr_save_local(long *status);
+
+#endif /* BAREMETAL_CRT_INTR_H_ */
---
+++ sys/intr.c
@@ -0,0 +1,30 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#include <errno.h>
+#include <inttypes.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include <bmetal/intr.h>
+#include <bmetal/arch.h>
+#include <bmetal/printk.h>
+
+void __intr_enable_local(void)
+{
+	__arch_intr_enable_local();
+}
+
+void __intr_disable_local(void)
+{
+	__arch_intr_disable_local();
+}
+
+void __intr_restore_local(const long status)
+{
+	__arch_intr_restore_local(status);
+}
+
+void __intr_save_local(long *status)
+{
+	__arch_intr_save_local(status);
+}
