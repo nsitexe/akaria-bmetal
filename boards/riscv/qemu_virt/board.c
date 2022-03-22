3352d7f4ec83868110883fcd7f7172d8b52e0ee4
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2022-03-22 17:17:58 +0900

    boards: qemu_virt: add configs for intc

--- boards/riscv/qemu_virt/board.c
+++ boards/riscv/qemu_virt/board.c
@@ -2,7 +2,9 @@
 
 #include <bmetal/device.h>
 #include <bmetal/init.h>
+#include <bmetal/bindings/intc/riscv/rv_priv.h>
 #include <bmetal/drivers/cpu.h>
+#include <bmetal/drivers/intc.h>
 #include <bmetal/drivers/uart.h>
 
 #define CPU_CONF(N)    \
@@ -37,6 +39,88 @@ static struct __cpu_device cpu[] = {
 	CPU_DEVICE(3),
 };
 
+#define INTC_CONF(N)    \
+	[N] = { \
+		{"cpu", 1, {UPTR("cpu" #N)}}, \
+		{0}, \
+	}
+
+#define INTC_DEVICE(N)    \
+	[N] = { \
+		.base = { \
+			.name = "rvintc" #N, \
+			.type_vendor = "riscv", \
+			.type_device = "priv1_10", \
+			.conf = rvintc_conf[N], \
+			.priv = &rvintc_priv[N], \
+		}, \
+	}
+
+const static struct __device_config rvintc_conf[][2] = {
+	INTC_CONF(0),
+	INTC_CONF(1),
+	INTC_CONF(2),
+	INTC_CONF(3),
+};
+
+static __intc_priv_t rvintc_priv[4];
+static struct __intc_device rvintc[] = {
+	INTC_DEVICE(0),
+	INTC_DEVICE(1),
+	INTC_DEVICE(2),
+	INTC_DEVICE(3),
+};
+
+const static struct __device_config clint_conf[] = {
+	{"reg", 1, {0x2000000}},
+	{"reg-size", 1, {0x1000}},
+	{"interrupts", 16, {
+		UPTR("rvintc0"), RV_IX_SIX,
+		UPTR("rvintc1"), RV_IX_SIX,
+		UPTR("rvintc2"), RV_IX_SIX,
+		UPTR("rvintc3"), RV_IX_SIX,
+		UPTR("rvintc0"), RV_IX_TIX,
+		UPTR("rvintc1"), RV_IX_TIX,
+		UPTR("rvintc2"), RV_IX_TIX,
+		UPTR("rvintc3"), RV_IX_TIX,
+		}},
+	{0},
+};
+
+static __intc_priv_t clint_priv;
+static struct __intc_device clint = {
+	.base = {
+		.name = "clint",
+		.type_vendor = "sifive",
+		.type_device = "clint0",
+		.conf = clint_conf,
+		.priv = &clint_priv,
+	}
+};
+
+const static struct __device_config plic_conf[] = {
+	{"reg", 1, {0xc000000}},
+	{"reg-size", 1, {0x2000000}},
+	{"interrupts", 8, {
+		UPTR("rvintc0"), RV_IX_EIX,
+		UPTR("rvintc1"), RV_IX_EIX,
+		UPTR("rvintc2"), RV_IX_EIX,
+		UPTR("rvintc3"), RV_IX_EIX,
+		}},
+	{0},
+};
+
+static __intc_priv_t plic_priv;
+static struct __intc_device plic = {
+	.base = {
+		.name = "plic",
+		.type_vendor = "sifive",
+		.type_device = "plic0",
+		.conf = plic_conf,
+		.priv = &plic_priv,
+	}
+};
+
 const static struct __device_config uart0_conf[] = {
 	{"reg", 1, {0x10000000}},
 	{"reg-size", 1, {0x100}},
@@ -57,7 +141,10 @@ static int board_qemu_virt_init(void)
 {
 	for (int i = 0; i < 4; i++) {
 		__cpu_add_device(&cpu[i], __bus_get_root());
+		__intc_add_device(&rvintc[i], __bus_get_root());
 	}
+	__intc_add_device(&plic, __bus_get_root());
+	__intc_add_device(&clint, __bus_get_root());
 	__uart_add_device(&uart0, __bus_get_root(), 1);
 
 	return 0;
