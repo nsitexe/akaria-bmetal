4dbca49a927e1dcc10661a7f4154b99c91ddcb04
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2022-04-04 13:19:59 +0900

    sys: fix prototype for printk like functions

--- include/bmetal/printk.h
+++ include/bmetal/printk.h
@@ -9,8 +9,15 @@
 
 typedef int (*__putc_func)(int c);
 
-int __attribute__((format(printf, 1, 2))) printk(const char *fmt, ...);
-int vprintk(const char *fmt, va_list ap);
+int kputchar(int c);
+int kputs(const char *s);
+int __attribute__((format(printf, 1, 2))) printk(const char *format, ...);
+int __attribute__((format(printf, 2, 3))) sprintk(char *buffer, const char *format, ...);
+int __attribute__((format(printf, 3, 4))) snprintk(char *buffer, size_t count, const char *format, ...);
+int vprintk(const char *format, va_list va);
+int vsprintk(char *buffer, const char *format, va_list va);
+int vsnprintk(char *buffer, size_t count, const char *format, va_list va);
+int __attribute__((format(printf, 3, 4))) fctprintk(void (*out)(char character, void *arg), void *arg, const char *format, ...);
 
 __putc_func __get_printk_out(void);
 int __set_printk_out(__putc_func f);
