d6a5f18c498448eeeedd02ee8302e548894dcb51
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2022-06-06 17:38:58 +0900

    test: refine test of pthread

--- test/pthread.c
+++ test/pthread.c
@@ -7,141 +7,135 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define NOP    __asm volatile("nop")
+int __arch_riscv_get_cpu_id(void);
 
-uint64_t get_cycle(void)
-{
-	uint64_t v;
-	const int mcycle = 0xb00;
-
-	__asm volatile("csrr %0, %1" : "=r"(v) : "i"(mcycle));
-
-	return v;
-}
-
-int test_args(int argc, char *argv[])
-{
-	if (argc <= 0) {
-		printf("argc is zero.\n");
-		return -1;
-	}
-	if (argv[0] == NULL) {
-		printf("argv[0] is NULL.\n");
-		return -1;
-	}
-
-	return 0;
-}
-
-int test_ctype(void)
-{
-	volatile char ch_a = 'a';
-	volatile char ch_cb = 'B';
-
-	if (isupper(ch_a)) {
-		printf("isupper('%c') result is incorrect.\n", ch_a);
-		return -1;
-	}
-	if (islower(ch_cb)) {
-		printf("islower('%c') result is incorrect.\n", ch_cb);
-		return -1;
-	}
-
-	return 0;
-}
+pthread_attr_t attr;
+pthread_t th[8];
 
 void *thread_main(void *arg)
 {
-	printf("thread!! argh:%d\n", (int)(intptr_t)arg);
+	int cpuid = __arch_riscv_get_cpu_id();
+	int v = (int)(intptr_t)arg * 10 + 1;
+
+	printf("%d: ---- thread step1 arg:%d %p\n", cpuid, v, &arg);
+	fflush(stdout);
+	printf("%d: ---- thread step2 arg:%d %p\n", cpuid, v, &arg);
+	for (int i = 0; i < 1000000; i++) {
+		fflush(stdout);
+	}
+	printf("%d: ---- thread step3 arg:%d %p\n", cpuid, v, &arg);
 	fflush(stdout);
 
 	return NULL;
 }
 
+void *parent_thread_main(void *arg)
+{
+	int cpuid = __arch_riscv_get_cpu_id();
+	int v = (int)(intptr_t)arg;
+	void *val, *ret;
+	int r;
+
+	for (int i = 1; i < 3; i++) {
+		r = pthread_create(&th[v + i], NULL, thread_main, (void *)(intptr_t)(v + i));
+		if (r) {
+			printf("%d: pthread_create: %s\n", cpuid, strerror(r));
+		}
+		fflush(stdout);
+	}
+
+	r = pthread_join(th[v + 1], &val);
+	if (r) {
+		printf("%d: pthread_join: %s\n", cpuid, strerror(r));
+	}
+	fflush(stdout);
+
+	ret = thread_main(arg);
+
+	r = pthread_join(th[v + 2], &val);
+	if (r) {
+		printf("%d: pthread_join: %s\n", cpuid, strerror(r));
+	}
+	fflush(stdout);
+
+	return ret;
+}
+
 int main(int argc, char *argv[], char *envp[])
 {
-	printf("%s: hello world!\n", argv[0]);
-
-	printf("pid:%d\n", getpid());
-	fflush(stdout);
-
-	if (test_args(argc, argv)) {
-		printf("test_args failed.\n");
-	}
-	if (test_ctype()) {
-		printf("%s: test_ctype failed.\n", argv[0]);
-	}
-
-	volatile uint64_t s = get_cycle();
-	volatile uint64_t e = get_cycle();
-	printf("clock1! %"PRIx64"\n", s);
-	printf("clock1! %"PRIx64"\n", e);
-
-	printf("argc: %d\n", argc);
-	for (int i = 0; i < argc; i++) {
-		printf("argv[%d]: %p\n", i, argv[i]);
-	}
-	printf("clock2! %"PRIx64"\n", get_cycle());
-	fflush(stdout);
-
-	pthread_t th[4];
-	pthread_attr_t attr;
+	int cpuid = __arch_riscv_get_cpu_id();
+	void *val;
 	int r;
+
+	printf("%s: test pthread\n", argv[0]);
+	fflush(stdout);
 
 	r = pthread_attr_init(&attr);
 	if (r) {
-		printf("pthread_attr_init: %s\n", strerror(r));
+		printf("%d: pthread_attr_init: %s\n", cpuid, strerror(r));
 	}
 
 	//r = pthread_attr_setstacksize(&attr, 32768);
 	//if (r) {
-	//	printf("pthread_attr_setstacksize: %s\n", strerror(r));
+	//	printf("%d: pthread_attr_setstacksize: %s\n", cpuid, strerror(r));
 	//}
 	//fflush(stdout);
 
-	r = pthread_create(&th[0], NULL, thread_main, (void *)11);
-	if (r) {
-		printf("pthread_create: %s\n", strerror(r));
+	printf("%d: -------- step1 2threads start\n", cpuid);
+	for (int i = 0; i < 2; i++) {
+		r = pthread_create(&th[i], NULL, thread_main, (void *)(intptr_t)i);
+		if (r) {
+			printf("%d: pthread_create: %s\n", cpuid, strerror(r));
+		}
+		fflush(stdout);
 	}
-	fflush(stdout);
-
-	r = pthread_create(&th[1], NULL, thread_main, (void *)21);
-	if (r) {
-		printf("pthread_create: %s\n", strerror(r));
-	}
-	fflush(stdout);
 
 	for (int i = 0; i < 2; i++) {
-		void *val;
-
 		r = pthread_join(th[i], &val);
 		if (r) {
-			printf("pthread_join: %s\n", strerror(r));
+			printf("%d: pthread_join: %s\n", cpuid, strerror(r));
+		}
+		fflush(stdout);
+	}
+	printf("%d: -------- step1 joined\n", cpuid);
+	fflush(stdout);
+
+	printf("%d: -------- step2 3threads start\n", cpuid);
+	fflush(stdout);
+	for (int i = 2; i < 5; i++) {
+		r = pthread_create(&th[i], NULL, thread_main, (void *)(intptr_t)i);
+		if (r) {
+			printf("%d: pthread_create: %s\n", cpuid, strerror(r));
 		}
 		fflush(stdout);
 	}
 
-	r = pthread_create(&th[2], NULL, thread_main, (void *)31);
-	if (r) {
-		printf("pthread_create: %s\n", strerror(r));
-	}
-	fflush(stdout);
-
-	r = pthread_create(&th[3], NULL, thread_main, (void *)41);
-	if (r) {
-		printf("pthread_create: %s\n", strerror(r));
-	}
-	fflush(stdout);
-
-	for (int i = 2; i < 4; i++) {
-		void *val;
-
+	for (int i = 2; i < 5; i++) {
 		r = pthread_join(th[i], &val);
 		if (r) {
-			printf("pthread_join: %s\n", strerror(r));
+			printf("%d: pthread_join: %s\n", cpuid, strerror(r));
 		}
 		fflush(stdout);
 	}
+	printf("%d: -------- step2 joined\n", cpuid);
+	fflush(stdout);
+
+	printf("%d: -------- step3 thread from thread start\n", cpuid);
+	fflush(stdout);
+	r = pthread_create(&th[5], NULL, parent_thread_main, (void *)(intptr_t)5);
+	if (r) {
+		printf("%d: pthread_create: %s\n", cpuid, strerror(r));
+	}
+	fflush(stdout);
+
+	r = pthread_join(th[5], &val);
+	if (r) {
+		printf("%d: pthread_join: %s\n", cpuid, strerror(r));
+	}
+	fflush(stdout);
+
+	printf("%d: -------- step3 joined\n", cpuid);
+	fflush(stdout);
 
 	return 0;
 }
