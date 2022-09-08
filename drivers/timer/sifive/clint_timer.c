24b86a028210c949ac0d6ad822657120464cb85b
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2022-09-08 18:18:54 +0900

    drivers: timer: add SiFive clint driver for timer part

--- drivers/timer/sifive/clint_timer.c
+++ drivers/timer/sifive/clint_timer.c
@@ -9,15 +9,63 @@
 #include <bmetal/init.h>
 #include <bmetal/intr.h>
 #include <bmetal/printk.h>
+#include <bmetal/drivers/clk.h>
+#include <bmetal/sys/time.h>
 
 /* Start address is 0x4000 of clint */
 #define REG_MTIMECMP(hart)    (0x0000 + (hart) * 8)
-#define REG_MTIME             0x3ff8
+#define REG_MTIME             0x7ff8
 
 struct timer_clint_priv {
 	struct __timer_device *timer;
+	struct __clk_device *clk_in;
+	int index_clk_in;
+	uint64_t freq_in;
 };
 CHECK_PRIV_SIZE_TIMER(struct timer_clint_priv);
+
+static int timer_clint_get_freq(struct __timer_device *tm, int index, uint64_t *freq)
+{
+	struct __device *dev = __timer_to_dev(tm);
+	struct timer_clint_priv *priv = dev->priv;
+
+	if (priv->freq_in == 0) {
+		__dev_err(dev, "input clock frequency is 0.\n");
+		return -EINVAL;
+	}
+
+	if (freq) {
+		*freq = priv->freq_in;
+	}
+
+	return 0;
+}
+
+static int timer_clint_get_raw(struct __timer_device *tm, int index, uint64_t *count)
+{
+	struct __device *dev = __timer_to_dev(tm);
+	uint64_t v;
+
+#ifdef CONFIG_64BIT
+	v = __device_read64(dev, REG_MTIME);
+#else
+	uint32_t vh1, vh2, vl;
+
+	do {
+		vh1 = __device_read64(dev, REG_MTIME);
+		vl = __device_read64(dev, REG_MTIME);
+		vh2 = __device_read64(dev, REG_MTIME);
+	} while (vh1 != vh2);
+
+	v = (vh1 << 32) | vl;
+#endif /* CONFIG_64BIT */
+
+	if (count) {
+		*count = v;
+	}
+
+	return 0;
+}
 
 static int timer_clint_add(struct __device *dev)
 {
@@ -37,6 +85,22 @@ static int timer_clint_add(struct __device *dev)
 		return r;
 	}
 
+	r = __clk_get_clk_from_config(dev, 0, &priv->clk_in, &priv->index_clk_in);
+	if (r) {
+		return r;
+	}
+
+	r = __clk_get_frequency(priv->clk_in, priv->index_clk_in, &priv->freq_in);
+	if (r) {
+		__dev_err(dev, "clock freq is unknown.\n");
+		return r;
+	}
+
+	r = __clk_enable(priv->clk_in, priv->index_clk_in);
+	if (r) {
+		return r;
+	}
+
 	return 0;
 }
 
@@ -52,6 +116,8 @@ const static struct __device_driver_ops timer_clint_dev_ops = {
 };
 
 const static struct __timer_driver_ops timer_clint_timer_ops = {
+	.get_freq = timer_clint_get_freq,
+	.get_raw = timer_clint_get_raw,
 };
 
 static struct __timer_driver timer_clint_drv = {
