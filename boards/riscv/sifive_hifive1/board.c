4f11c80c0546781e3559cfbd2bd1b2c586d493e4
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2022-09-30 12:40:07 +0900

    boards: hifive1: follow clk and uart settings

--- boards/riscv/sifive_hifive1/board.c
+++ boards/riscv/sifive_hifive1/board.c
@@ -2,6 +2,7 @@
 
 #include <bmetal/device.h>
 #include <bmetal/init.h>
+#include <bmetal/drivers/clk.h>
 #include <bmetal/drivers/cpu.h>
 #include <bmetal/drivers/uart.h>
 
@@ -21,39 +22,62 @@ static struct __cpu_device cpu0 = {
 	},
 };
 
+const static struct __device_config hfclk_conf[] = {
+	PROP("frequency", 16000000),
+	{0},
+};
+
+static __clk_priv_t hfclk_priv;
+static struct __clk_device hfclk = {
+	.base = {
+		.name = "hfclk",
+		.type_vendor = "none",
+		.type_device = "clk_fixed",
+		.conf = hfclk_conf,
+		.priv = &hfclk_priv,
+	},
+};
+
 const static struct __device_config uart0_conf[] = {
 	PROP("reg", 0x10013000),
 	PROP("reg-size", 0x1000),
+	PROP("clocks", UPTR("hfclk"), 0),
 	{0},
 };
 
 const static struct __device_config uart1_conf[] = {
 	PROP("reg", 0x10023000),
 	PROP("reg-size", 0x1000),
+	PROP("clocks", UPTR("hfclk"), 0),
 	{0},
 };
 
+static __uart_priv_t uart0_priv;
 static struct __uart_device uart0 = {
 	.base = {
 		.name = "uart0",
 		.type_vendor = "sifive",
 		.type_device = "uart0",
 		.conf = uart0_conf,
+		.priv = &uart0_priv,
 	},
 };
 
+static __uart_priv_t uart1_priv;
 static struct __uart_device uart1 = {
 	.base = {
 		.name = "uart1",
 		.type_vendor = "sifive",
 		.type_device = "uart0",
 		.conf = uart1_conf,
+		.priv = &uart1_priv,
 	},
 };
 
 static int board_hifive1_init(void)
 {
 	__cpu_add_device(&cpu0, __bus_get_root());
+	__clk_add_device(&hfclk, __bus_get_root());
 	__uart_add_device(&uart0, __bus_get_root(), 1);
 	__uart_add_device(&uart1, __bus_get_root(), 1);
 
