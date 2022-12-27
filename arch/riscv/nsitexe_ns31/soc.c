672998999b1fdff052761db4665495b0cc09223b
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2022-12-27 22:20:16 +0900

    riscv: nsitexe-ns31: add initial support

---
+++ arch/riscv/nsitexe_ns31/soc.c
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#include <bmetal/device.h>
+#include <bmetal/init.h>
+
+static int soc_nsitexe_ns31_init(void)
+{
+	return 0;
+}
+
+define_init_func(soc_nsitexe_ns31_init);
