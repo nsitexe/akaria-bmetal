fbe142034105bb9b510224d4b90400b902750855
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2023-02-02 01:55:38 +0900

    sys: add syscall wrappers to cast type of arg correctly
    This patch adds macro to cast narrow type arguments (ex. int on
    64bits arch) from native type of register.
    
    NG case is passing 0x00000000_ffffffff as an argment.
    User applications expected this value is evaluated as -1 (all f)
    but no one extends signed bit. So system call function will receive
    4 billion.

--- include/bmetal/syscall.h
+++ include/bmetal/syscall.h
@@ -16,6 +16,55 @@
 
 #define SYSCALL_P(num, func)    [(num)] = (__syscall_func_t)(func)
 
+#define SYSCALL_WRAP0(name) \
+	intptr_t __sys_##name(void); \
+	static inline intptr_t __sys_wrap_##name(void) \
+	{ \
+		return __sys_##name(); \
+	}
+
+#define SYSCALL_WRAP1(name, t1, a1) \
+	intptr_t __sys_##name(t1 a1); \
+	static inline intptr_t __sys_wrap_##name(intptr_t a1) \
+	{ \
+		return __sys_##name((t1)a1); \
+	}
+
+#define SYSCALL_WRAP2(name, t1, a1, t2, a2) \
+	intptr_t __sys_##name(t1 a1, t2 a2); \
+	static inline intptr_t __sys_wrap_##name(intptr_t a1, intptr_t a2) \
+	{ \
+		return __sys_##name((t1)a1, (t2)a2); \
+	}
+
+#define SYSCALL_WRAP3(name, t1, a1, t2, a2, t3, a3) \
+	intptr_t __sys_##name(t1 a1, t2 a2, t3 a3); \
+	static inline intptr_t __sys_wrap_##name(intptr_t a1, intptr_t a2, intptr_t a3) \
+	{ \
+		return __sys_##name((t1)a1, (t2)a2, (t3)a3); \
+	}
+
+#define SYSCALL_WRAP4(name, t1, a1, t2, a2, t3, a3, t4, a4) \
+	intptr_t __sys_##name(t1 a1, t2 a2, t3 a3, t4 a4); \
+	static inline intptr_t __sys_wrap_##name(intptr_t a1, intptr_t a2, intptr_t a3, intptr_t a4) \
+	{ \
+		return __sys_##name((t1)a1, (t2)a2, (t3)a3, (t4)a4); \
+	}
+
+#define SYSCALL_WRAP5(name, t1, a1, t2, a2, t3, a3, t4, a4, t5, a5) \
+	intptr_t __sys_##name(t1 a1, t2 a2, t3 a3, t4 a4, t5 a5); \
+	static inline intptr_t __sys_wrap_##name(intptr_t a1, intptr_t a2, intptr_t a3, intptr_t a4, intptr_t a5) \
+	{ \
+		return __sys_##name((t1)a1, (t2)a2, (t3)a3, (t4)a4, (t5)a5); \
+	}
+
+#define SYSCALL_WRAP6(name, t1, a1, t2, a2, t3, a3, t4, a4, t5, a5, t6, a6) \
+	intptr_t __sys_##name(t1 a1, t2 a2, t3 a3, t4 a4, t5 a5, t6 a6); \
+	static inline intptr_t __sys_wrap_##name(intptr_t a1, intptr_t a2, intptr_t a3, intptr_t a4, intptr_t a5, intptr_t a6) \
+	{ \
+		return __sys_##name((t1)a1, (t2)a2, (t3)a3, (t4)a4, (t5)a5, (t6)a6); \
+	}
+
 #define __NEW_UTS_LEN 64
 #define __PAGE_SIZE   4096
 
@@ -34,45 +83,45 @@ typedef intptr_t (*__syscall_func_t)(intptr_t no, intptr_t a, intptr_t b, intptr
 
 intptr_t __sys_unknown(intptr_t number, intptr_t a, intptr_t b, intptr_t c, intptr_t d, intptr_t e, intptr_t f);
 
-intptr_t __sys_uname(struct new_utsname *name);
-intptr_t __sys_prlimit64(pid_t pid, int resource, const struct rlimit64 *new_lim, struct rlimit64 *old_lim);
-intptr_t __sys_getuid(void);
-intptr_t __sys_geteuid(void);
-intptr_t __sys_getgid(void);
-intptr_t __sys_getegid(void);
-intptr_t __sys_getpid(void);
-intptr_t __sys_gettid(void);
-intptr_t __sys_clock_gettime32(clockid_t clock_id, struct timespec32 *tp);
-intptr_t __sys_clock_settime32(clockid_t clock_id, const struct timespec32 *tp);
-intptr_t __sys_clock_gettime64(clockid_t clock_id, struct timespec64 *tp);
-intptr_t __sys_clock_settime64(clockid_t clock_id, const struct timespec64 *tp);
-intptr_t __sys_gettimeofday(struct timeval *tp, void *tzp);
-intptr_t __sys_settimeofday(const struct timeval *tp, const void *tzp);
-intptr_t __sys_getrandom(void *buf, size_t buflen, unsigned int flags);
-intptr_t __sys_close(int fd);
-intptr_t __sys_read(int fd, void *buf, size_t count);
-intptr_t __sys_write(int fd, const void *buf, size_t count);
-intptr_t __sys_writev(int fd, const struct iovec *iov, int iovcnt);
+SYSCALL_WRAP1(uname, struct new_utsname *, name);
+SYSCALL_WRAP4(prlimit64, pid_t, pid, int, resource, const struct rlimit64 *, new_lim, struct rlimit64 *, old_lim);
+SYSCALL_WRAP0(getuid);
+SYSCALL_WRAP0(geteuid);
+SYSCALL_WRAP0(getgid);
+SYSCALL_WRAP0(getegid);
+SYSCALL_WRAP0(getpid);
+SYSCALL_WRAP0(gettid);
+SYSCALL_WRAP2(clock_gettime32, clockid_t, clock_id, struct timespec32 *, tp);
+SYSCALL_WRAP2(clock_settime32, clockid_t, clock_id, const struct timespec32 *, tp);
+SYSCALL_WRAP2(clock_gettime64, clockid_t, clock_id, struct timespec64 *, tp);
+SYSCALL_WRAP2(clock_settime64, clockid_t, clock_id, const struct timespec64 *, tp);
+SYSCALL_WRAP2(gettimeofday, struct timeval *, tp, void *, tzp);
+SYSCALL_WRAP2(settimeofday, const struct timeval *, tp, const void *, tzp);
+SYSCALL_WRAP3(getrandom, void *, buf, size_t, buflen, unsigned int, flags);
+SYSCALL_WRAP1(close, int, fd);
+SYSCALL_WRAP3(read, int, fd, void *, buf, size_t, count);
+SYSCALL_WRAP3(write, int, fd, const void *, buf, size_t, count);
+SYSCALL_WRAP3(writev, int, fd, const struct iovec *, iov, int, iovcnt);
 void *__brk_area_start(void);
 void *__brk_area_end(void);
-intptr_t __sys_brk(void *addr);
+SYSCALL_WRAP1(brk, void *, addr);
 void *__heap_area_start(void);
 void *__heap_area_end(void);
-intptr_t __sys_mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
-intptr_t __sys_munmap(void *addr, size_t length);
-intptr_t __sys_madvise(void *addr, size_t length, int advice);
-intptr_t __sys_mprotect(void *addr, size_t length, int prot);
+SYSCALL_WRAP6(mmap, void *, addr, size_t, length, int, prot, int, flags, int, fd, off_t, offset);
+SYSCALL_WRAP2(munmap, void *, addr, size_t, length);
+SYSCALL_WRAP3(madvise, void *, addr, size_t, length, int, advice);
+SYSCALL_WRAP3(mprotect, void *, addr, size_t, length, int, prot);
 #if defined(CONFIG_RISCV)
-intptr_t __sys_clone(unsigned long flags, void *child_stack, void *ptid, void *tls, void *ctid);
+SYSCALL_WRAP5(clone, unsigned long, flags, void *, child_stack, void *, ptid, void *, tls, void *, ctid);
 #endif
-intptr_t __sys_futex32(int *uaddr, int op, int val, const struct timespec32 *timeout, int *uaddr2, int val3);
-intptr_t __sys_futex64(int *uaddr, int op, int val, const struct timespec64 *timeout, int *uaddr2, int val3);
-intptr_t __sys_set_robust_list(void *head, size_t len);
-intptr_t __sys_set_tid_address(int *tidptr);
-intptr_t __sys_exit_group(int status);
-intptr_t __sys_exit(int status);
-intptr_t __sys_reboot(int magic, int magic2, int cmd);
-intptr_t __sys_context_switch(void);
+SYSCALL_WRAP6(futex32, int *, uaddr, int, op, int, val, const struct timespec32 *, timeout, int *, uaddr2, int, val3);
+SYSCALL_WRAP6(futex64, int *, uaddr, int, op, int, val, const struct timespec64 *, timeout, int *, uaddr2, int, val3);
+SYSCALL_WRAP2(set_robust_list, void *, head, size_t, len);
+SYSCALL_WRAP1(set_tid_address, int *, tidptr);
+SYSCALL_WRAP1(exit_group, int, status);
+SYSCALL_WRAP1(exit, int, status);
+SYSCALL_WRAP3(reboot, int, magic, int, magic2, int, cmd);
+SYSCALL_WRAP0(context_switch);
 
 #endif /* !__ASSEMBLER__ */
 
