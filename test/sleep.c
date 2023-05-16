646d9d0ff69f43afe810fd885213effdf65babaf
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2023-05-16 14:26:04 +0900

    test: add nanosleep test

--- test/sleep.c
+++ test/sleep.c
@@ -27,9 +27,44 @@ int test_usleep(void)
 	}
 
 	timersub(&ed, &st, &elapse);
-	printf("gettimeofday: start  : %lld.%06d[s]\n",
+	printf("test_usleep: start  : %lld.%06d[s]\n",
 		(long long)st.tv_sec, (int)st.tv_usec);
-	printf("gettimeofday: elapsed: %d.%06d[s]\n",
+	printf("test_usleep: elapsed: %d.%06d[s]\n",
+		(int)elapse.tv_sec, (int)elapse.tv_usec);
+
+	return 0;
+}
+
+int test_nanosleep(void)
+{
+	struct timeval st, ed, elapse;
+	struct timespec ts;
+	int r;
+
+	r = gettimeofday(&st, NULL);
+	if (r) {
+		perror("gettimeofday(nanosleep, start)");
+		return -1;
+	}
+
+	ts.tv_sec = 0;
+	ts.tv_nsec = 1000000;
+	r = nanosleep(&ts, &ts);
+	if (r) {
+		perror("nanosleep()");
+		return -1;
+	}
+
+	r = gettimeofday(&ed, NULL);
+	if (r) {
+		perror("gettimeofday(nanosleep, end)");
+		return -1;
+	}
+
+	timersub(&ed, &st, &elapse);
+	printf("test_nanosleep: start  : %lld.%06d[s]\n",
+		(long long)st.tv_sec, (int)st.tv_usec);
+	printf("test_nanosleep: elapsed: %d.%06d[s]\n",
 		(int)elapse.tv_sec, (int)elapse.tv_usec);
 
 	return 0;
@@ -42,6 +77,11 @@ int main(int argc, char *argv[], char *envp[])
 	printf("%s: test sleep start\n", argv[0]);
 
 	r = test_usleep();
+	if (r) {
+		ret = r;
+	}
+
+	r = test_nanosleep();
 	if (r) {
 		ret = r;
 	}
